245472b42d03a2557c29141ad8e7368eae1ecc92
Nagaraja Balakrishna <nbalakrishna@manh.com>
2020-05-18 19:51:49 +0530

    Add state transition from pre-auth to auth and auth-error

--- src/test/java/com/njb/msscssm/PaymentServiceImplTest.java
+++ src/test/java/com/njb/msscssm/PaymentServiceImplTest.java
@@ -1,17 +1,19 @@
 package com.njb.msscssm;
 
-import static org.junit.jupiter.api.Assertions.fail;
-
 import java.math.BigDecimal;
 
 import javax.transaction.Transactional;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.statemachine.StateMachine;
 
 import com.njb.msscssm.domain.Payment;
+import com.njb.msscssm.domain.PaymentEvent;
+import com.njb.msscssm.domain.PaymentState;
 import com.njb.msscssm.repository.PaymentRepository;
 import com.njb.msscssm.services.PaymentService;
 
@@ -32,7 +34,7 @@ class PaymentServiceImplTest {
 	}
 
 	@Transactional
-	@Test
+	//@Test
 	void testPreAutorize() {
 
 		Payment savedPayment = paymentService.newPayment(payment);
@@ -45,6 +47,26 @@ class PaymentServiceImplTest {
 
 		System.out.println("Should be PRE_AUTH or PRE_AUTH_DECLINED!");
 		System.out.println(preAuthPayement);
+
+	}
+
+	@RepeatedTest(10)
+	@Transactional
+	void testFullAutorize() {
+
+		Payment savedPayment = paymentService.newPayment(payment);
+
+		StateMachine<PaymentState, PaymentEvent> preAuthSm = paymentService.preAutorize(savedPayment.getId());
+
+		if (preAuthSm.getState().getId() == PaymentState.PRE_AUTH) {
+			System.out.println("Payment is pre-authorised!");
+			StateMachine<PaymentState, PaymentEvent> authSM = paymentService.authorizePayment(savedPayment.getId());
+			System.out.println("result of Authorization: " + authSM.getState().getId());
+
+		} else {
+			System.out.println("Pre-auth failed!");
+		}
+
 	}
 
 }
